1aa1b7b59b374047a01319a909806171ef952975
Norman Soares <jp062758@gmail.com>
2025-10-10 01:28:50 +0100

    Fix MongoDB options - Remove deprecated bufferMaxEntries and bufferCommands

--- api/aliexpress-callback.js
+++ api/aliexpress-callback.js
@@ -60,8 +60,6 @@ async function connectToMongoDB() {
             maxPoolSize: 10,
             serverSelectionTimeoutMS: 5000,
             socketTimeoutMS: 45000,
-            bufferMaxEntries: 0,
-            bufferCommands: false,
             retryWrites: true,
             retryReads: true,
         });
